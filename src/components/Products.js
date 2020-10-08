2bb7cdef24e8f1e805cc56fb60033fff67bef20e
Sam Tarullo <sam@Sams-Air.attlocal.net>
2020-10-08 15:16:50 -0500

    finished with main tasks

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
-// Components
+import {ProductContext} from '../contexts/ProductContext';
 import Product from './Product';
 
-const Products = props => {
+
+// Components
+
+
+const Products = () => {
+	const {products, addItem} = useContext(ProductContext);
 	return (
 		<div className="products-container">
-			{props.products.map(product => (
+			{products.map(product => (
 				<Product
 					key={product.id}
 					product={product}
-					addItem={props.addItem}
+					addItem={addItem}
 				/>
 			))}
 		</div>
